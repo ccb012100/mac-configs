3b88c92be9eea9ba2738fe167bef810f5ac296ed
Christopher Bocardo <ccb012100@users.noreply.github.com>
2019-07-10 16:16:25 -0500

    added aliases; added zsh_aliases to rsync

--- rsync-files.zsh
+++ rsync-files.zsh
@@ -5,6 +5,7 @@ rsync -ti --progress /Users/cbocardo/.emacs.d/macOS.el /Users/cbocardo/ccb012100
 rsync -ti --progress /Users/cbocardo/.emacs.d/packages.el /Users/cbocardo/ccb012100/emacs/.emacs.d/
 rsync -ti --progress /Users/cbocardo/.emacs.d/settings.el /Users/cbocardo/ccb012100/emacs/.emacs.d/
 rsync -ti --progress /Users/cbocardo/.zshrc /Users/cbocardo/ccb012100/mac-configs/
+rsync -ti --progress /Users/cbocardo/.zsh_aliases /Users/cbocardo/ccb012100/mac-configs/
 rsync -ti --progress /Users/cbocardo/.zsh_unalias /Users/cbocardo/ccb012100/mac-configs/
 rsync -ti --progress "/Users/cbocardo/Library/Application Support/Code/User/settings.json" /Users/cbocardo/ccb012100/vscode-settings/macos/
 rsync -ti --progress "/Users/cbocardo/Library/Application Support/Code/User/keybindings.json" /Users/cbocardo/ccb012100/vscode-settings/macos/
