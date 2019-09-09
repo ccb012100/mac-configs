5b51741de1bd35486c805fd3ac57c1ba8116c948
Christopher Bocardo <ccb012100@users.noreply.github.com>
2019-09-09 11:07:28 -0500

    added tmux config

--- rsync-files.zsh
+++ rsync-files.zsh
@@ -7,17 +7,20 @@ rsync -ti --progress ~/.emacs.d/packages.el ~/ccb012100/emacs/.emacs.d/
 rsync -ti --progress ~/.emacs.d/settings.el ~/ccb012100/emacs/.emacs.d/
 
 # zsh
-rsync -ti --progress ~/.zshrc ~/ccb012100/mac-configs/
-rsync -ti --progress ~/.zsh-aliases ~/ccb012100/mac-configs/
-rsync -ti --progress ~/.zsh-unalias ~/ccb012100/mac-configs/
+rsync -ti --progress ~/.zshrc ~/ccb012100/mac-configs/Users/cbocardo/
+rsync -ti --progress ~/.zsh-aliases ~/ccb012100/mac-configs/Users/cbocardo/
+rsync -ti --progress ~/.zsh-unalias ~/ccb012100/mac-configs/Users/cbocardo/
 
 # Vim/Neovim
-rsync -ti --progress ~/.config/nvim/init.vim ~/ccb012100/mac-configs/
-rsync -ti --progress ~/.vim/vimrc ~/ccb012100/mac-configs/
+rsync -ti --progress ~/.config/nvim/init.vim \
+      ~/ccb012100/mac-configs/Users/cbocardo/.config/nvim/
+
+rsync -ti --progress ~/.vim/vimrc ~/ccb012100/mac-configs/Users/cbocardo/.vim/
 
 # VS Code
 rsync -ti --progress ~/Library/Application\ Support/Code/User/settings.json \
       ~/ccb012100/vscode-settings/macos/
+
 rsync -ti --progress ~/Library/Application\ Support/Code/User/keybindings.json \
       ~/ccb012100/vscode-settings/macos/
 
@@ -26,18 +29,21 @@ rsync -ti --progress ~/Library/VisualStudio/8.0/KeyBindings/Custom.mac-kb.xml \
       ~/ccb012100/mac-configs/visual-studio/
 
 # Alacritty
-rsync -ti --progress ~/.config/alacritty/alacritty.yml ~/ccb012100/mac-configs/
+rsync -ti --progress ~/.config/alacritty/alacritty.yml \
+      ~/ccb012100/mac-configs/Users/cbocardo/.config/alacritty/
 
 # tmux
-rsync -ti --progress ~/.tmux.conf ~/ccb012100/linux-configs/
+rsync -ti --progress ~/.tmux.conf ~/ccb012100/mac-configs/Users/cbocardo/
 
 # bat
-rsync -ti --progress ~/.config/bat/config/bat.conf ~/ccb012100/linux-configs/
+rsync -ti --progress ~/.config/bat/config/bat.conf \
+      ~/ccb012100/mac-configs/Users/cbocardo/.config/bat/
 
 # TODO: add for JetBrains Rider - copy to git folder
 
 # ranger
-rsync -ti --progress ~/.config/ranger/rc.conf ~/ccb012100/mac-configs/
+rsync -ti --progress ~/.config/ranger/rc.conf \
+      ~/ccb012100/mac-configs/Users/cbocardo/.config/ranger/
 
 # Spectacle
 rsync -ti --progress ~/Library/Application\ Support/Spectacle/Shortcuts.json \
